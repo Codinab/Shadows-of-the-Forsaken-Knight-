33d7925b230ad3f34153e87150dd89ab40c085da
Moify64 <145990148+Moify64@users.noreply.github.com>
2023-09-27 15:19:48 +0200

    partially fixed the jump of the spider
    would still be confused when is it grounded when on the edge of a block

--- Assets/Scripts/SmallSpiderChasing.cs
+++ Assets/Scripts/SmallSpiderChasing.cs
@@ -9,11 +9,15 @@ public class SmallSpiderChasing : MonoBehaviour
     private GameObject _player;
     [SerializeField] int Speed;
     [SerializeField] float ReactionTime;
-    [SerializeField] int JumpStrenght;
+    [SerializeField] int JumpStrength;
+    [SerializeField] GameObject GroundCheckBody;
     Rigidbody2D _rb;
     float lastLookRight;
     float lastLookLeft;
-
+    float lastJump;
+    float peakJump;
+    bool jumped;
+    [SerializeField] LayerMask Ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +25,14 @@ public class SmallSpiderChasing : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         lastLookLeft = 0;
         lastLookRight = 0;
+        lastJump = 0;
+        peakJump = transform.position.y;
+        jumped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_player == null) return;
         //chasing
         if (GroundCheck())
         {
@@ -46,24 +52,57 @@ public class SmallSpiderChasing : MonoBehaviour
                     lastLookLeft = Time.time;
                 }
             }
+            //Debug.Log(_rb.velocity);
         }
-        
-        //jumping in on trigger
+        if(GroundCheck())
+        Debug.Log($"lossy: {GroundCheckBody.transform.lossyScale} local: {GroundCheckBody.transform.localScale}");
+        //jumping on trigger
+        if (jumped)
+        {
+            if (peakJump > transform.position.y)
+            {
+                if (_player.transform.position.x > transform.position.x)
+                {
+                    _rb.velocity += new Vector2(1, 0);
+                }
+                if (_player.transform.position.x < transform.position.x)
+                {
+                    _rb.velocity += new Vector2(-1, 0);
+                }
+                peakJump = transform.position.y;
+                jumped = false;
+                Debug.Log("Jumped");
+            }
+            if (peakJump < transform.position.y)
+            {
+                peakJump = transform.position.y;
+            }
+            //Debug.Log(_rb.velocity);
+        }
         
     }
     private bool GroundCheck()
     {
-        return _rb.velocity.y == 0;
+        //return _rb.velocity.y == 0;
+        return Physics2D.OverlapBox(GroundCheckBody.transform.position, GroundCheckBody.transform.lossyScale, 0f,Ground);
+        //return Physics2D.OverlapBox(groundCheck.position, _boxSize, 0f, whatIsGround);
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.tag == "Ground")
+        
+        if (other.tag == "Ground" && GroundCheck() && lastJump + JumpStrength/3 < Time.time)
         {
             Jump();
+            lastJump = Time.time;
         }
     }
+    
     private void Jump()
     {
-        _rb.velocity = new Vector2(_rb.velocity.x, JumpStrenght);
+        if (GroundCheck())
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, JumpStrength);
+            jumped = true;
+        }
     }
 }
