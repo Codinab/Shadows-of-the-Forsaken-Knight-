5b6e4e96978e88e45d76708123c1df90de42e6b6
Moify64 <145990148+Moify64@users.noreply.github.com>
2023-10-03 17:30:22 +0200

    added both aggressive and nonaggressive behaviour

--- Assets/Scripts/Enemies/FlyChasing.cs
+++ Assets/Scripts/Enemies/FlyChasing.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class FlyChasing : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private GameObject _player;
-    //used for reaction time
-    private float lastLookLeft;
-    private float lastLookRight;
+    private float _startPositionX;
+    private bool _goingRight;
 
     //chase speed
-    [SerializeField] float HorizontalVelocity;
+    [SerializeField] float HorizontalVelocity = 3;
     //approch (descend) speed and min height
-    [SerializeField] float ChaseVelocity;
+    [SerializeField] float ChaseVelocity = 4;
     //how far is it when it starts descending toward the player
-    [SerializeField] float DescendDistance;
+    [SerializeField] float AgressiveRadius = 5;
     //max height and normal speed
-    [SerializeField] float NormalVerticalVelocity;
-    [SerializeField] float NormalHoverHeight;
+    [SerializeField] float VerticalVelocity = 2.5f;
+    [SerializeField] float HoverHeight = 5;
     //dont wiggle too much damp
-    [SerializeField] float HoverDamp=0.01f;
-    //time needed to turn around
-    [SerializeField] float ReactionTime;
+    [SerializeField] float HoverDamp = 0.1f;
+    [SerializeField] float RoamRadius = 5;
 
 
     // Start is called before the first frame update
@@ -31,8 +31,8 @@ public class FlyChasing : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _player = GameObject.FindGameObjectWithTag("Player");
-        lastLookLeft = 0;
-        lastLookRight = 0;
+        _startPositionX = transform.position.x;
+        _goingRight = true;
     }
 
     // Update is called once per frame
@@ -40,62 +40,77 @@ public class FlyChasing : MonoBehaviour
     {
         float playerX = _player.transform.position.x;
         float flyX = transform.position.x;
-        BasicMove(playerX,flyX);
 
-        
-        if (math.abs(playerX - flyX) < DescendDistance)//descend
+        if (Vector2.Distance(_player.transform.position, transform.position) < AgressiveRadius)//descend
         {
             ChasePlayer();
+
         }
         else
         {
+            
+            BasicRoam();
             MaintainHeight();
         }
     }
-    private void BasicMove(float playerXPosition,float flyXPosition) 
+    private void BasicRoam()
     {
-        if (playerXPosition > flyXPosition &&
-                (lastLookLeft + ReactionTime < Time.time || lastLookLeft == 0))
+
+        if (_goingRight && DidntPassOnTheRight())
         {
             _rb.velocity = new Vector2(HorizontalVelocity, _rb.velocity.y);
-            lastLookRight = Time.time;
         }
-
-        if (playerXPosition < flyXPosition &&
-            (lastLookRight + ReactionTime < Time.time || lastLookRight == 0))
+        else if (!_goingRight && DidntPassOnTheLeft())
         {
             _rb.velocity = new Vector2(-HorizontalVelocity, _rb.velocity.y);
-            lastLookLeft = Time.time;
         }
+        else
+        {
+            _goingRight = !_goingRight;
+        }
+
+
+
+        //if ()
+        //{
+        //    _goingRight = true;
+        //}
+        //else if ()
+        //{
+        //    _goingRight = false;
+        //}
+        //Debug.Log($"{transform.position.x + RoamRadius} < {_startPositionX} go left");
+        //Debug.Log($"{transform.position.x - RoamRadius} < {_startPositionX} go right");
+    }
+    private bool DidntPassOnTheRight()
+    {
+        return transform.position.x - RoamRadius < _startPositionX;
+    }
+    private bool DidntPassOnTheLeft()
+    {
+        return transform.position.x + RoamRadius > _startPositionX;
     }
     private void MaintainHeight()
     {
-        
+
         float vel;
         float height;
-        //if (close)
-        //{
-        //    vel = CloseVerticalVelocity;
-        //    height = CloseHoverHeight;
-        //}
-        //else
-        //{
-            vel = NormalVerticalVelocity;
-            height = NormalHoverHeight;
-        //}
+        vel = VerticalVelocity;
+        height = HoverHeight;
+
 
         RaycastHit2D hit = CastRay();
 
         if (hit.distance < height)//too low, fly up
         {
-            _rb.velocity = new Vector2(_rb.velocity.x,vel);
+            _rb.velocity = new Vector2(_rb.velocity.x, vel);
         }
-        else if(hit.distance > height + HoverDamp)//too high, fly down
+        else if (hit.distance > height + HoverDamp)//too high, fly down
         {
             _rb.velocity = new Vector2(_rb.velocity.x, -vel);
         }
         else //perfect stay there
-        { 
+        {
             _rb.velocity = new Vector2(_rb.velocity.x, 0);
         }
     }
@@ -104,7 +119,8 @@ public class FlyChasing : MonoBehaviour
 
         Vector2 direction = _player.transform.position - transform.position;
         direction.Normalize();
-        _rb.velocity = direction*ChaseVelocity;
+        _rb.velocity = direction * ChaseVelocity;
+
     }
     private RaycastHit2D CastRay()
     {
@@ -116,7 +132,7 @@ public class FlyChasing : MonoBehaviour
         {
             if (hit.collider.tag == "Ground")
             {
-                return hit;  
+                return hit;
             }
         }
         return rcHits[rcHits.Length - 1];
