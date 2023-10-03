37e74e9ce204266e04b46af5d02a06a9fea1d415
Moify64 <145990148+Moify64@users.noreply.github.com>
2023-10-03 16:25:16 +0200

    chasing is finalized

--- Assets/Scripts/Enemies/FlyChasing.cs
+++ Assets/Scripts/Enemies/FlyChasing.cs
@@ -14,8 +14,7 @@ public class FlyChasing : MonoBehaviour
     //chase speed
     [SerializeField] float HorizontalVelocity;
     //approch (descend) speed and min height
-    [SerializeField] float CloseHoverHeight;
-    [SerializeField] float CloseVerticalVelocity;
+    [SerializeField] float ChaseVelocity;
     //how far is it when it starts descending toward the player
     [SerializeField] float DescendDistance;
     //max height and normal speed
@@ -46,11 +45,11 @@ public class FlyChasing : MonoBehaviour
         
         if (math.abs(playerX - flyX) < DescendDistance)//descend
         {
-            MaintainHeight(true);
+            ChasePlayer();
         }
         else
         {
-            MaintainHeight(false);
+            MaintainHeight();
         }
     }
     private void BasicMove(float playerXPosition,float flyXPosition) 
@@ -69,36 +68,29 @@ public class FlyChasing : MonoBehaviour
             lastLookLeft = Time.time;
         }
     }
-    private void MaintainHeight(bool close)
+    private void MaintainHeight()
     {
-
+        
         float vel;
         float height;
-        if (close)
-        {
-            vel = CloseVerticalVelocity;
-            height = CloseHoverHeight;
-        }
-        else
-        {
+        //if (close)
+        //{
+        //    vel = CloseVerticalVelocity;
+        //    height = CloseHoverHeight;
+        //}
+        //else
+        //{
             vel = NormalVerticalVelocity;
             height = NormalHoverHeight;
-        }
-        
-        //origin right under the body
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - transform.localScale.y);
-        //cast it down
-        RaycastHit2D rcHit = Physics2D.Raycast(origin, Vector2.down);
-        Debug.Log(rcHit.collider.name);
-        if(rcHit.collider.tag == "Player" || rcHit.collider.tag == "Collider") // player there just go on him
-        {
-            _rb.velocity = new Vector2(0, -vel);
-        }
-        else if (rcHit.distance < height)//too low, fly up
+        //}
+
+        RaycastHit2D hit = CastRay();
+
+        if (hit.distance < height)//too low, fly up
         {
             _rb.velocity = new Vector2(_rb.velocity.x,vel);
         }
-        else if(rcHit.distance > height + HoverDamp)//too high, fly down
+        else if(hit.distance > height + HoverDamp)//too high, fly down
         {
             _rb.velocity = new Vector2(_rb.velocity.x, -vel);
         }
@@ -106,5 +98,27 @@ public class FlyChasing : MonoBehaviour
         { 
             _rb.velocity = new Vector2(_rb.velocity.x, 0);
         }
+    }
+    private void ChasePlayer()
+    {
+
+        Vector2 direction = _player.transform.position - transform.position;
+        direction.Normalize();
+        _rb.velocity = direction*ChaseVelocity;
+    }
+    private RaycastHit2D CastRay()
+    {
+        //origin right under the body
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - transform.localScale.y);
+        //cast it down
+        RaycastHit2D[] rcHits = Physics2D.RaycastAll(origin, Vector2.down);
+        foreach (RaycastHit2D hit in rcHits)
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                return hit;  
+            }
+        }
+        return rcHits[rcHits.Length - 1];
     }
 }
