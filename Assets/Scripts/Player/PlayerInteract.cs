35fb511b5b502a02d355acf08c1683b5400ac0db
Moify64 <145990148+Moify64@users.noreply.github.com>
2023-10-17 18:27:15 +0200

    left: interact, pick up, test

--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -2,15 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickup : MonoBehaviour
+public class PlayerInteract : MonoBehaviour
 {
-    [SerializeField] float interactDistance=0.7f;
     // Start is called before the first frame update
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, interactDistance);
-    }
     void Start()
     {
         
